908d7debb1c5c7971adaeaeafe9982c78427ebe7
Karalinga <49670475+Karalinga@users.noreply.github.com>
2021-09-12 15:38:39 +1000

    Update usb_mouse_test.ino

--- usb_mouse/usb_mouse_test.ino
+++ usb_mouse/usb_mouse_test.ino
@@ -34,66 +34,6 @@ void setup()
 void loop()
 {
   Usb.Task();
-  readIn();
-  doTask();
-}
-
-void readIn() {
-  commandList[0] = 0;
-  charLimit = buffer - 1;
-  counter = 0;
-  inputs = 0;
-
-  if (Serial.available()) {
-    while (Serial.available()) {
-      if (inputs > charLimit - 1) {
-        inputs = charLimit;
-      }
-      commandList[inputs] = Serial.read();
-      inputs ++;
-      counter ++;
-    }
-    if (inputs > charLimit) {
-      inputs = charLimit;
-    }
-    commandList[inputs] = 0;
-  }
-  char * split;
-  x = atoi(strtok(commandList, ","));
-  y = atoi(strtok(NULL, ","));
-  shoot = atoi(strtok(NULL, ","));
-}
-
-void doTask() {
-  if (x != 0 || y != 0) {
-    moveMouse(x, y);
-  }
-  if (shoot == 1) {
-    Mouse.begin();
-    Mouse.press();
-    delay(5);
-    Mouse.release();
-    delay(5);
-    Mouse.end();
-  }
-}
-void moveMouse(long x, long y) {
-  fullDist = max(abs(x), abs(y));
-  count = (int) (fullDist / 127);
-  moveX = x / (count + 1);
-  moveY = y / (count + 1);
-  for (int i = 0; i < count; i++) {
-    Mouse.begin();
-    Mouse.move(moveX, moveY);
-    Mouse.end();
-  }
-  remainderX = x - (moveX * count);
-  remainderY = y - (moveY * count);
-  if (remainderX != 0 || remainderY != 0) {
-    Mouse.begin();
-    Mouse.move(remainderX, remainderY);
-    Mouse.end();
-  }
 }
 void mouseCommand(uint8_t mc, int rp){
     Mouse.begin();
