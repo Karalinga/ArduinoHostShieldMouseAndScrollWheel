93da1b78911fe3978ce017950a241d6c336fb684
Karalinga <49670475+Karalinga@users.noreply.github.com>
2021-09-12 15:39:37 +1000

    Update usb_mouse_test.ino

--- usb_mouse/usb_mouse_test.ino
+++ usb_mouse/usb_mouse_test.ino
@@ -9,10 +9,6 @@ protected:
   void OnRightButtonDown  (MOUSEINFO *mi);
   void OnMiddleButtonUp (MOUSEINFO *mi);
   void OnMiddleButtonDown (MOUSEINFO *mi);
-  //void OnX1ButtonUp (MOUSEINFO *mi);
-  //void OnX1ButtonDown (MOUSEINFO *mi);
-  //void OnX2ButtonUp (MOUSEINFO *mi);
-  //void OnX2ButtonDown (MOUSEINFO *mi);
   void OnWheelMove (MOUSEINFO *mi);
 };
 MouseRptParser                               Prs;
@@ -25,9 +21,6 @@ void setup()
         Serial.println("OSC did not start.");
 
     delay( 200 );
-
-    //next_time = millis() + 5000;
-
     HidMouse.SetReportParser(0,(HIDReportParser*)&Prs);
 }
 
@@ -58,33 +51,6 @@ void MouseRptParser::OnMiddleButtonDown (MOUSEINFO *mi){mouseCommand(MOUSE_MIDDL
 void MouseRptParser::OnWheelMove (MOUSEINFO *mi)
 {
   Mouse.begin();
-  //Serial.println(mi->dZ);
   Mouse.move(0, 0, mi->dZ);
   Mouse.end();
 };
-/*
-void MouseRptParser::OnX1ButtonUp (MOUSEINFO *mi)
-{
-    Mouse.begin();
-    //Mouse.release(MOUSE_BACK);
-    Mouse.end();
-};
-void MouseRptParser::OnX1ButtonDown (MOUSEINFO *mi)
-{
-    Mouse.begin();
-    //Mouse.press(MOUSE_BACK);
-    Mouse.end();
-};
-void MouseRptParser::OnX2ButtonUp (MOUSEINFO *mi)
-{
-    Mouse.begin();
-    //Mouse.release(MOUSE_FORWARD);
-    Mouse.end();
-};
-void MouseRptParser::OnX2ButtonDown (MOUSEINFO *mi)
-{
-    Mouse.begin();
-    //Mouse.press(MOUSE_FORWARD);
-    Mouse.end();
-};
-*/
